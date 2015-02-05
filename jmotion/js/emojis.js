4df7a499d15054cf914de55ef080ca89f35e5638
alimjan.ablikim <alimjan.ablikim@gmail.com>
2015-02-05 11:18:46 +0100

    chatjs0.4
    add motion face versie

---
+++ jmotion/js/emojis.js
@@ -0,0 +1,8 @@
+$.emojiarea.path = 'jmotion/png';
+$.emojiarea.icons = {
+	':smile:'     : 'smile.png',
+	':angry:'     : 'angry.png',
+	':flushed:'   : 'flushed.png',
+	':neckbeard:' : 'neckbeard.png',
+	':laughing:'  : 'laughing.png'
+};
