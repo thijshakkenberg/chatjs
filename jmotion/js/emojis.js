ce71ebbabebd66e1b613e507cf86847b69fc1f8b
alimjan.ablikim <alimjan.ablikim@gmail.com>
2015-02-05 13:46:40 +0100

    voor de revert
    voor de revert

--- jmotion/js/emojis.js
+++
@@ -1,8 +0,0 @@
-$.emojiarea.path = 'jmotion/png';
-$.emojiarea.icons = {
-	':smile:'     : 'smile.png',
-	':angry:'     : 'angry.png',
-	':flushed:'   : 'flushed.png',
-	':neckbeard:' : 'neckbeard.png',
-	':laughing:'  : 'laughing.png'
-};
